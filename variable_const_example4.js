af5e83f366fd50e367aaad159ecdd61643fa0ddf
JESSE BITRUS <jessebitrus2@gmail.com>
2026-07-02 11:16:17 +0100

    Delete variable_const_example4.js

--- variable_const_example4.js
+++
@@ -1,5 +0,0 @@
-//Example4:	Arrow Functions: Stores a functional reference securely, preventing accidental replacement of logic.
-
-	const calculateTotal = (price, tax) => price + (price * tax);
- 
-	console.log(calculateTotal(100, 0.05)); // 105
