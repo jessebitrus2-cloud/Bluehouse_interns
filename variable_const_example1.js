e352a68298bc738dd551c232a4cc8bc1f3dd006d
JESSE BITRUS <jessebitrus2@gmail.com>
2026-07-02 11:13:01 +0100

    Delete variable_const_example1.js

--- variable_const_example1.js
+++
@@ -1,9 +0,0 @@
-//Example1: Primitive Values. 
-// This Locks the binding of a basic data type. 
-// Reassignment throws a TypeError.
-
- 	const TAX_RATE = 0.05;
- 
-	// This will cause an error:
-	// TAX_RATE = 0.08; 
-    // // TypeError: Assignment to constant variable.
