19a91357418c9143abcb61b82087eed00172450d
JESSE BITRUS <jessebitrus2@gmail.com>
2026-07-02 11:09:22 +0100

    Delete alert_instance2.js

--- alert_instance2.js
+++
@@ -1,8 +0,0 @@
-//Example2 of alert instance.
-//Displaying Variables
-//You can pass a variable into the function to display dynamic data.
- 
-
-let username = "Alex";
-alert("Hello, " + username + "! Your account is active.");
-
