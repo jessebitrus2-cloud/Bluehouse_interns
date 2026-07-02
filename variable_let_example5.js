eec22c709caaab8dc23cb25684572e2cf95f9ea8
JESSE BITRUS <jessebitrus2@gmail.com>
2026-07-02 11:19:23 +0100

    Delete variable_let_example5.js

--- variable_let_example5.js
+++
@@ -1,9 +0,0 @@
-//Example5: Temporal Dead Zone (TDZ)
-//Throws a reference error if accessed before declaration, preventing accidental early usage.
- 
-// This will cause an error:
-// console.log(greeting); // ReferenceError: Cannot access 'greeting' before initialization
-
- 
-let greeting = "Hello!";
-console.log(greeting); // Works: "Hello!"
