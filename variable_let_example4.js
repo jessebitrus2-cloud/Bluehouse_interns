45fdd1cd93b54a6b9974008caf030c0ec60ecf49
JESSE BITRUS <jessebitrus2@gmail.com>
2026-07-02 11:19:37 +0100

    Delete variable_let_example4.js

--- variable_let_example4.js
+++
@@ -1,6 +0,0 @@
-//Example4: Dynamic Data Types
-//Allows changing both the value and the underlying data type at runtime.
-
-let response = "Loading..."; // Starts as a string
-response = 200; // Allowed: changes to a number
-response = { success: true }; // Allowed: changes to an object
