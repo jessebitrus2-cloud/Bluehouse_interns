0aed9b8157d3c3066eb99967b18793958cda8bf6
JESSE BITRUS <jessebitrus2@gmail.com>
2026-07-02 11:16:50 +0100

    Delete variable_var_example5.js

--- variable_var_example5.js
+++
@@ -1,8 +0,0 @@
-//Example5: Loop Scope Leakage: Leaks loop counters into the surrounding scope, which frequently causes issues in logic and timing.
-
-for (var i = 0; i < 3; i++) {
-  // Loop runs
-}
- 
-console.log(i); // Works: outputs 3 (The variable leaked outside the loop)
- 
