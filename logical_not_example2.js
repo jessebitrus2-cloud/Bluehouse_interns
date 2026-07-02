0d074c91065dcc3812e1733523b028669cb279aa
JESSE BITRUS <jessebitrus2@gmail.com>
2026-07-02 11:11:05 +0100

    Delete logical_not_example2.js

--- logical_not_example2.js
+++
@@ -1,9 +0,0 @@
-//Logical NOT (!) 
-//Example2
-function processPayment(amount) { 
-// If amount does NOT exist or is zero, exit early 
-   if (!amount) { 
-   return "Error: Invalid amount"; 
-   } 
-   return "Payment processed successfully"; 
-   }
