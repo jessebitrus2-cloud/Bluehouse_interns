19102cbc99bac886e0538c543f5ec717d196de1c
JESSE BITRUS <jessebitrus2@gmail.com>
2026-07-02 11:16:02 +0100

    Delete variable_let_example1.js

--- variable_let_example1.js
+++
@@ -1,6 +0,0 @@
-//Example1: Reassigning Values
-//Allows updating the data stored in the variable as program conditions change
-
-let score = 0;
-score = 10; // Allowed: value changes to 10
-score = score + 5; // Allowed: value changes to 15
