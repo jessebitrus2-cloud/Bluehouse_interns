348c7e81d1fc05d1efb1eb19c37d808d6726a1f7
JESSE BITRUS <jessebitrus2@gmail.com>
2026-07-02 11:17:07 +0100

    Delete variable_var_example4.js

--- variable_var_example4.js
+++
@@ -1,7 +0,0 @@
-//Example4: Global Window Binding
-//Attaches the variable directly onto the global window object when declared outside a function. 
-
-var globalConfig = "dark_mode";
- 
-// In browser environments:
-console.log(window.globalConfig); // Outputs: "dark_mode"
